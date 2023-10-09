e2169b28f7004b41f51c4fb164b51e709740c688
Diego Quintero <diquintero@unal.edu.co>
2023-10-08 22:00:13 -0500

    create network attempt

--- scratch/main.cc
+++ scratch/main.cc
@@ -3,10 +3,54 @@
 #include "ns3/mobility-module.h"
 #include "ns3/internet-module.h"
 #include "ns3/point-to-point-module.h"
+#include "ns3/wifi-module.h"
+#include "ns3/yans-wifi-helper.h"
 
 using namespace ns3;
 
 NS_LOG_COMPONENT_DEFINE ("DynamicNetworkSimulation");
+
+void CreateWiFiNetwork(Ptr<Node> node, NodeContainer& otherNodes) {
+  WifiHelper wifi;
+  wifi.SetStandard(WIFI_STANDARD_80211b);
+
+  YansWifiPhyHelper wifiPhy;
+  YansWifiChannelHelper wifiChannel;
+  wifiChannel.SetPropagationDelay("ns3::ConstantSpeedPropagationDelayModel");
+  wifiChannel.AddPropagationLoss("ns3::FriisPropagationLossModel");
+  wifiPhy.SetChannel(wifiChannel.Create());
+
+  // Add a mac and disable rate control
+  WifiMacHelper wifiMac;
+  std::string phyMode("DsssRate11Mbps");
+  wifi.SetRemoteStationManager("ns3::ConstantRateWifiManager",
+                                "DataMode",
+                                StringValue(phyMode),
+                                "ControlMode",
+                                StringValue(phyMode));
+
+  double m_txp{7.5};
+
+  wifiPhy.Set("TxPowerStart", DoubleValue(m_txp));
+  wifiPhy.Set("TxPowerEnd", DoubleValue(m_txp));
+
+  NodeContainer centerNode;
+  centerNode.Add(node);
+
+  wifiMac.SetType("ns3::AdhocWifiMac");
+  NetDeviceContainer centerDevices = wifi.Install(wifiPhy, wifiMac, centerNode);
+  NetDeviceContainer otherDevices = wifi.Install(wifiPhy, wifiMac, otherNodes);
+
+  // Create a P2P link between each otherNode and the centerNode
+  for (uint32_t i = 0; i < otherNodes.GetN(); ++i) {
+    PointToPointHelper p2p;
+    p2p.SetDeviceAttribute("DataRate", StringValue("100Mbps"));
+    p2p.SetDeviceAttribute("Mtu", UintegerValue(1400));
+    p2p.SetChannelAttribute("Delay", TimeValue(NanoSeconds(6560)));
+
+    NetDeviceContainer link = p2p.Install(otherNodes.Get(i), centerNode.Get (0));
+  }
+}
 
 std::queue<Task> GenerateTaskQueue() {
   std::queue<Task> taskQueue;
@@ -24,17 +68,20 @@ std::queue<Task> GenerateTaskQueue() {
   return taskQueue;
 }
 
-void PublishTask(Ptr<Node> node) {
+void PublishTask(Ptr<Node> node, NodeContainer& L1_nodes) {
   std::queue<Task> tqueue = node->GetTasks();
   if (!tqueue.empty()) {
     Task task = tqueue.front();
+
+    CreateWiFiNetwork(node, L1_nodes);
+
     NS_LOG_INFO("Published Task: " << "Node=" << node->GetId() <<  " Threads=" << task.threads << " RAM=" << task.ram << " Time=" << task.time << " Tasks=" << tqueue.size());
     tqueue.pop();
     node->SetTasks(tqueue);
   }
 
   // Schedule the next task processing event
-  Simulator::Schedule(Seconds(1), &PublishTask, node);
+  Simulator::Schedule(Seconds(1), &PublishTask, node, L1_nodes);
 }
 
 void LogNodePositions_L1 (NodeContainer& nodes)
@@ -89,7 +136,7 @@ int main (int argc, char *argv[])
     uint32_t ram = r_ram->GetInteger (4, 16);
     std::queue<Task> queue = GenerateTaskQueue();
     L2_nodes.Create (1, threads, ram, queue);
-    Simulator::Schedule(Seconds(1), &PublishTask, L2_nodes.Get (i));
+    Simulator::Schedule(Seconds(1), &PublishTask, L2_nodes.Get (i), L1_nodes);
   }
 
   MobilityHelper mobility;
@@ -108,7 +155,6 @@ int main (int argc, char *argv[])
 
   Simulator::Schedule (Seconds (1), &LogNodePositions_L1, std::ref(L1_nodes)); 
   Simulator::Schedule (Seconds (1), &LogNodePositions_L2, std::ref(L2_nodes));  
-  //Simulator::Schedule(Seconds(1), &PublishTask, std::ref(taskQueue));
   Simulator::Stop (Seconds (100));
   Simulator::Run ();
 
